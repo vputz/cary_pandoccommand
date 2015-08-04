cc6e3c3a907fdd2975c931f9763917bd4123b73b
vputz <vputz@nyx.net>
2015-08-04 11:20:20 +0000

    initial import

---
+++ README.md
@@ -0,0 +1,20 @@
+Cary-pandoccommand
+-------------------
+
+A simple cary command to run the unix `pandoc` command.  Obviously
+you need pandoc installed on your system.  At the moment this simply
+converts the attached markdown file into html, docx, epub, and pdf
+formats and mails them back (in the future I will try to handle
+attachments, command-line options, etc).
+
+Configuration
+-------------
+
+In your configuration (usually a `local_conf.py` file), have the following:
+
+```
+PANDOC_CONFIG = dict(PANDOC_PATH='/path/to/pandoc/command',
+                     TEXLIVE_PATH='/path/to/texlive/executables')
+from cary_pandoccommand import PandocCommand
+COMMANDS = {..."pandoc": (PandocCommand, PANDOC_CONFIG)...}
+```
---
+++ __init__.py
@@ -0,0 +1,80 @@
+from carycommand import CaryCommand, CaryAction
+import os
+import logging
+from subprocess import call
+
+"""
+A command to run pandoc on the attachment(s)
+"""
+
+class PandocCommand(CaryCommand):
+
+    @property
+    def name(self):
+        return "pandoc"
+
+    @property
+    def description(self):
+        return "Use pandoc to convert markdown documents"
+
+    @property
+    def required_attachments(self):
+        return ["sourcedoc.md", "support1.file", "support2.file..."]
+
+    def _create_action(self, parsed_message):
+        return PandocAction(parsed_message)
+
+
+class PandocAction(CaryAction):
+
+    def __init__(self, parsed_message):
+        super().__init__(parsed_message)
+
+    def validate_command(self):
+        # pandoc should at least have one attachment and we hope it is valid markdown
+        if len(self._attachments) > 0:
+            self.command_is_valid = True
+        else:
+            self.invalid_command_response = """
+I'm sorry, pandoc requires an input document, and I did not see one in your email.
+"""
+
+    def execute_action(self):
+        source_path = self._attachments[0]
+        self._output_filenames = []
+        for filetype, suffix in (('epub', 'epub'),
+                                 ('docx', 'docx'),
+                                 ('latex', 'pdf'),
+                                 ('html', 'html')):
+            self.convert_to(filetype, suffix)
+
+    def output_path(self, suffix):
+        base_fnam = os.path.split(self._attachments[0])[-1]
+        fnam_root, fnam_ext = os.path.splitext(base_fnam)
+        return os.path.join(self.output_dir, fnam_root+"."+suffix)
+
+    def convert_to(self, filetype, suffix):
+        source = self._attachments[0]
+        target = self.output_path(suffix)
+        command_list = [self.config['PANDOC_PATH'],
+                        '-f', 'markdown', '-t', filetype, '-o',
+                        target, source]
+        self._output_filenames.append(target)
+        call_result = call(command_list, env=self.execution_environment())
+        logging.log(logging.INFO, " ".join(command_list)
+                    + "::" + str(call_result) + "\n")
+
+    def execution_environment(self):
+        result = os.environ.copy()
+        result['PATH'] = self.config['TEXLIVE_PATH']+':'+os.environ['PATH']
+        return result
+
+    @property
+    def response_subject(self):
+        return "Pandoc: your transcribed versions of {0}".format(
+            os.path.split(self._attachments[0])[-1])
+
+    @property
+    def response_body(self):
+        return "Please find enclosed the transcribed versions you requested of document '{0}'.".format(
+            os.path.split(self._attachments[0])[-1])
